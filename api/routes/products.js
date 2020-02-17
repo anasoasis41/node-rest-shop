4f25e28e06c75af0f93bd6e6314a67f9c71f2c59
Anas RIAHI <anas.riahi@smile-maroc.com>
2020-02-17 18:50:42 +0100

    MongoDB and Mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,47 +1,110 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require("mongoose");
 
+const Product = require('../models/product');
+
+// get All products
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET request to /products'
-    });
-});
+    Product.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        //   if (docs.length >= 0) {
+        res.status(200).json(docs);
+        //   } else {
+        //       res.status(404).json({
+        //           message: 'No entries found'
+        //       });
+        //   }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
 
+  // Add product
 router.post('/', (req, res, next) => {
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    };
-    res.status(200).json({
-        message: 'Handling POST request to /products',
-        createdProduct: product
+    const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
+  });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Handling POST requests to /products",
+        createdProduct: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
 });
 
+// get product
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    if (id == 'special') {
-        res.status(200).json({
-            message: 'You discovered the special ID',
-            id: id
-        });
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID',
-            id: id
-        });
-    }
-});
-
-router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated products'
+    Product.findById(id)
+    .exec()
+    .then(doc => {
+      console.log("From database", doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
 });
 
+// Update product
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+// Delete product
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted products'
+    const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
 });
 
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
@@ -26,6 +27,16 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+mongoose.connect(
+    "mongodb+srv://node-shop:" + 
+    process.env.MONGO_ATLASS_PW + 
+    "@node-rest-shop-u9rh3.mongodb.net/test?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+    );
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
