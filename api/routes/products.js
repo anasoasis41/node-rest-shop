19f846fbee8369a5fa3da948521c7abf53364699
Anas RIAHI <anas.riahi@smile-maroc.com>
2020-02-18 18:09:10 +0100

    Uploading an Image to product

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require("mongoose");
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, './uploads/');
+  },
+  filename: function(req, file, callback) {
+    callback(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const filterFile = (req, file, callback) => {
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    callback(null, true);
+  } else {
+    callback(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: filterFile
+});
 
 const Product = require('../models/product');
 
 // get All products
 router.get('/', (req, res, next) => {
     Product.find()
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then(docs => {
         console.log(docs);
@@ -17,6 +44,7 @@ router.get('/', (req, res, next) => {
             return {
               name: doc.name,
               price: doc.price,
+              productImage: doc.productImage,
               _id: doc._id,
               request: {
                 type: "GET",
@@ -42,11 +70,12 @@ router.get('/', (req, res, next) => {
   });
 
   // Add product
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('productImage'), (req, res, next) => {
     const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price
+    price: req.body.price,
+    productImage: req.file.path
   });
   product
     .save()
@@ -77,6 +106,7 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
+    .select('name price _id productImage')
     .exec()
     .then(doc => {
       console.log("From database", doc);
